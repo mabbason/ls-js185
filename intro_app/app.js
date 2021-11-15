25bca9f395c2f3cc89ba926edcdaf8e0157a85c8
Miles Abbason <miles.abbason@gmail.com>
2021-11-15 14:28:50 -0500

    Worked through intro, at beginning of lesson 2.4

---
+++ intro_app/app.js
@@ -0,0 +1,16 @@
+const { Client } = require('pg');
+let client = new Client({ database: 'films' });
+
+async function logQuery(queryText) {
+  await client.connect();
+
+  let data = await client.query(queryText);
+  
+  console.log(data.rows[2].count);
+  client.end();
+};
+
+logQuery(`SELECT count(id) FROM films
+WHERE duration < 110
+GROUP BY genre;`);
+
